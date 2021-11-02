37cd6a86fef42a83d779d2dde382c689c6818cd0
Fred Lahde <fred@kloud.software>
2021-11-02 23:09:08 +0100

    make entry clone

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Entry {
    pub id:      String,
    pub title:   String,
